6d79dd5a08f0b5f18e195a3f731dcef5d53dca39
kordano <konrad.kuehne@rocketmail.com>
2014-02-25 21:05:16 +0100

    tiny change to bootstrap, revamp modal dialog

--- src/cljs/nidaba/client.cljs
+++ src/cljs/nidaba/client.cljs
@@ -20,40 +20,21 @@
 (enable-console-print!)
 
 
-(def app-state (atom {:appointment
-                      [{:date (js/Date. 2014 3 11) :client-id :1000 :price 25 :hours 3 :payed true}
-                       {:date (js/Date. 2014 2 11) :client-id :1001 :price 32 :hours 3 :payed true}
-                       {:date (js/Date. 2013 12 6) :client-id :1000 :price 11 :hours 5 :payed false}
-                       {:date (js/Date. 2014 4 15) :client-id :1000 :price 30 :hours 1 :payed true}
-                       {:date (js/Date. 2014 3 21) :client-id :1002 :price 25 :hours 2 :payed true}
-                       {:date (js/Date. 2014 5 3) :client-id :1002 :price 45 :hours 2 :payed false}
-                       {:date (js/Date. 2014 2 25) :client-id :1003 :price 35 :hours 3 :payed true}]
-                      :clients
-                      {:1000 {:name "Picard" :hourly-rate 11}
-                       :1001 {:name "Riker" :hourly-rate 6}
-                       :1002 {:name "Data" :hourly-rate 17}
-                       :1003 {:name "Troi" :hourly-rate 3}
-                       :1004 {:name "Crusher" :hourly-rate 12}}}))
-
-;; --- ui helper ---
-
-(defn overlay []
-  (let [el (.getElementById js/document "overlay")]
-    (if (= "visible" (.-visibility (.-style el)))
-      (set! (.-visibility (.-style el)) "hidden")
-      (set! (.-visibility (.-style el)) "visible"))))
-
-
-(defn show-menu [e]
-  (let [menu (aget (.-childNodes (.-parentNode (.-target e))) 1)]
-    (do
-      (.stopPropagation e)
-      (if (.contains (.-classList menu) "is-selected")
-        (.remove (.-classList menu) "is-selected")
-        (.add (.-classList menu) "is-selected")))))
-
-
-
+(def app-state
+  (atom {:appointment
+         [{:date (js/Date. 2014 3 11) :client-id :1000 :price 25 :hours 3 :payed true}
+          {:date (js/Date. 2014 2 11) :client-id :1001 :price 32 :hours 3 :payed true}
+          {:date (js/Date. 2013 12 6) :client-id :1000 :price 11 :hours 5 :payed false}
+          {:date (js/Date. 2014 4 15) :client-id :1000 :price 30 :hours 1 :payed true}
+          {:date (js/Date. 2014 3 21) :client-id :1002 :price 25 :hours 2 :payed true}
+          {:date (js/Date. 2014 5 3) :client-id :1002 :price 45 :hours 2 :payed false}
+          {:date (js/Date. 2014 2 25) :client-id :1003 :price 35 :hours 3 :payed true}]
+         :clients
+         {:1000 {:name "Picard" :hourly-rate 11}
+          :1001 {:name "Riker" :hourly-rate 6}
+          :1002 {:name "Data" :hourly-rate 17}
+          :1003 {:name "Troi" :hourly-rate 3}
+          :1004 {:name "Crusher" :hourly-rate 12}}}))
 
 
 ;; --- state helper ---
@@ -86,7 +67,97 @@
            (fn [x] (if x "yes" "no")))))))
 
 
+
 ;; --- view ---
+
+(defn appointment-modal [app owner]
+  (dom/div
+   #js {:className "modal fade"
+        :id "add-appointment-modal"
+        :tabindex "-1"
+        :role "dialog"
+        :aria-labelledby "add-appointment-modal-label"
+        :aria-hidden "true"}
+
+   (dom/div
+    #js {:className "modal-dialog"}
+
+    (dom/div
+     #js {:className "modal-content"}
+
+     (dom/div
+      #js {:className "modal-header"}
+
+      (dom/button
+       #js {:type "button"
+            :className "close"
+            :data-dismiss "modal"
+            :aria-hidden "true"}
+       "\u00D7")
+
+      (dom/h4
+       #js {:className "modal-title"
+            :id "add-appointment-modal-label"}
+       "Add new Appointment"))
+
+     (dom/div
+      #js {:className "modal-body"}
+
+      (dom/form
+       #js {:role "form"}
+
+       (dom/div
+        #js {:className "form-group"}
+
+        (dom/label
+         #js {:for "appointment-input-date"}
+         "Date")
+        (dom/input
+         #js {:type "date"
+              :className "form-control"
+              :id "appointment-input-date"
+              :placeholder "YYYY-MM-DD"})
+
+        (dom/label
+         #js {:for "appointment-input-hours"}
+         "Hours")
+        (dom/input
+         #js {:className "form-control"
+              :type "number"
+              :id "appointment-input-hours"})
+
+        (dom/label
+         #js {:for "appointment-input-id"}
+         "ID")
+        (dom/select
+         #js {:className "form-control"
+              :id "appointment-input-id"}
+         (doall
+          (map #(dom/option nil (str %)) (clients app))))
+
+
+        (dom/label
+         #js {:for "appointment-input-price"}
+         "Price")
+        (dom/input
+         #js {:type "number"
+              :className "form-control"
+              :id "appointment-input-price"}))))
+
+     (dom/div
+      #js {:className "modal-footer"}
+
+      (dom/button
+       #js {:type "button"
+            :className "btn btn-default"
+            :data-dismiss "modal"}
+       "Close")
+      (dom/button
+       #js {:type "button"
+            :className "btn btn-primary"}
+       "Energize"))))))
+
+
 
 (defn appointment-view [appointment owner]
   (reify
@@ -105,100 +176,36 @@
       (dom/div
        nil
 
-       (dom/div
-        #js {:className "cotainer-input" :id "overlay"}
-        (dom/div
-         nil
-
-         (dom/a
-          #js {:className "overlay-header"}
-          "Substanz ist Form und Inhalt.")
-
-         (dom/input
-          #js {:className "overlay-input"
-               :placeholder "Date"})
-
-         (dom/input
-          #js {:className "overlay-input"
-               :placeholder "Price"})
-
-         ;; --- client menu ---
-         (dom/li
-          #js {:className "menubar-item"
-               :onClick (fn [e] (show-menu e))}
-          (dom/a
-           #js {:className "menubar-item-target"}
-           "Select client ...")
-          (apply dom/ul #js {:className "menu"}
-                 (map
-                  #(dom/li
-                    #js {:className "menu-item"}
-                    (dom/a
-                     #js {:className "menu-item-target"
-                          :href "#"
-                          :onClick (fn [e] (.log js/console (.-target e)))
-                          }
-                     %))
-                  (clients app))))
-
-         ;; --- hours menu ---
-         (dom/li
-          #js {:className "menubar-item"
-               :onClick (fn [e] (show-menu e))}
-          (dom/a
-           #js {:className "menubar-item-target"}
-           "Select hours ...")
-          (apply dom/ul #js {:className "menu"}
-                 (map
-                  #(dom/li
-                    #js {:className "menu-item"}
-                    (dom/a
-                     #js {:className "menu-item-target"
-                          :onClick (fn [e] (.log js/console (.-target e)))
-                          :href "#"}
-                     (str %)))
-                  (range 1 10))))
-
-
-         (dom/div
-          #js {:className "overlay-nav"}
-
-          (dom/button
-           #js {:className "overlay-button"
-                :onClick (fn [] (overlay))}
-           "Cancel")
-
-          (dom/button
-           #js {:className "overlay-button"
-                :onClick (fn [] (overlay))}
-           "Ok"))))
-
        ;; --- appointment container ---
+       (dom/h1
+        #js {:className "page-header"} "Appointments")
+
+       (dom/button
+        #js {:className "btn btn-primary"
+             :data-toggle "modal"
+             :data-target "#add-appointment-modal"}
+        "Add Appointment")
+
+       ;; --- modal dialog
+       (appointment-modal app owner)
+
        (dom/div
-        #js {:className "container"}
+        #js {:className "table-responsive"}
 
-        (dom/div
-         #js {:className "container-header"}
+        (dom/table
+         #js {:className "table table-striped"}
 
-         (dom/a nil "Appointment List")
+         (dom/thead
+          nil
+          (dom/tr
+           nil
+           (dom/th nil "Date")
+           (dom/th nil "Price")
+           (dom/th nil "Name")
+           (dom/th nil "Hours")
+           (dom/th nil "payed?")))
 
-         (dom/button
-          #js {:className "header-button" :onClick (fn [e] (overlay))}
-          "Add appointment"))
-
-
-        (dom/div
-         #js {:className "container-list"}
-
-         (apply dom/table nil
-
-                (dom/tr nil
-                        (dom/th nil "Date")
-                        (dom/th nil "Price")
-                        (dom/th nil "Name")
-                        (dom/th nil "Hours")
-                        (dom/th nil "payed?"))
-
+         (apply dom/tbody nil
                 (om/build-all appointment-view (appointment app)))))))))
 
 
